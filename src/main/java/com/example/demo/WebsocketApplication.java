031b96d710a9fd46b8f69b4a4e12f9093bf029e2
zhangran <1qaz!QAZ2wsx@WSX>
2021-05-13 11:33:01 +0800

    始建

---
+++ src/main/java/com/example/demo/WebsocketApplication.java
@@ -0,0 +1,16 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * websocket 启动服务类
+ */
+@SpringBootApplication
+public class WebsocketApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebsocketApplication.class, args);
+    }
+
+}
